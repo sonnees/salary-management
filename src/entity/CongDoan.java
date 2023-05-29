70ef608f192e9174273fbfec2b922253ff97b00b
InInNek <79012320+inin-panda@users.noreply.github.com>
2023-05-29 14:21:18 +0700

    upload entityCongDoan

--- src/entity/CongDoan.java
+++ src/entity/CongDoan.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.util.Objects;
-
 public class CongDoan {
 	private String maCD;
 	private String tenCD;
@@ -10,7 +8,9 @@ public class CongDoan {
 	private int soLuong;
 	private SanPham sanPham;
 	private boolean hoanThanh;
+
 	
+
 	public CongDoan(String maCD, String tenCD, int doUuTien, long donGia, int soLuong, SanPham sanPham,
 			boolean hoanThanh) {
 		super();
@@ -23,9 +23,8 @@ public class CongDoan {
 		this.hoanThanh = hoanThanh;
 	}
 	
-	public CongDoan() {
-		super();
-	}
+	
+
 	public CongDoan(String tenCD, int doUuTien, long donGia, int soLuong, SanPham sanPham, boolean hoanThanh) {
 		super();
 		this.tenCD = tenCD;
@@ -36,30 +35,22 @@ public class CongDoan {
 		this.hoanThanh = hoanThanh;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(maCD);
+
+
+	public CongDoan() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CongDoan other = (CongDoan) obj;
-		return Objects.equals(maCD, other.maCD);
-	}
+
 	public String getMaCD() {
 		return maCD;
 	}
-	public void setMaCD(String maCD) {
-		this.maCD = maCD;
-	}
+
+	
 	public String getTenCD() {
 		return tenCD;
 	}
+
 	public void setTenCD(String tenCD) {
 		if(tenCD==null)
 			System.err.println("Tên công đoạn không được để trống");
@@ -105,12 +96,48 @@ public class CongDoan {
 			System.err.println("Sản phẩm không được để trống");
 		this.sanPham = sanPham;
 	}
+
+	
 	public boolean isHoanThanh() {
 		return hoanThanh;
 	}
+
 	public void setHoanThanh(boolean hoanThanh) {
 		this.hoanThanh = hoanThanh;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "CongDoan [maCD=" + maCD + ", tenCD=" + tenCD + ", doUuTien=" + doUuTien + ", donGia=" + donGia
+				+ ", soLuong=" + soLuong + ", sanPham=" + sanPham + ", hoanThanh=" + hoanThanh + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((maCD == null) ? 0 : maCD.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CongDoan other = (CongDoan) obj;
+		if (maCD == null) {
+			if (other.maCD != null)
+				return false;
+		} else if (!maCD.equals(other.maCD))
+			return false;
+		return true;
+	}
+	public void setMaCD(String maCD) {
+		this.maCD = maCD;
+	}
 	
 }
